b3d6a8c4270e3ab9343168edd4217fdcec246c27
vincentic <limingxin0729@foxmail.com>
2026-08-15 19:12:12 +0800

    Fix Java todo entity accessors

--- todo-list-app/backend-java/src/main/java/cn/lim/todolistservice/entity/Todo.java
+++ todo-list-app/backend-java/src/main/java/cn/lim/todolistservice/entity/Todo.java
@@ -3,6 +3,7 @@ package cn.lim.todolistservice.entity;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.IdType;
 import lombok.Data;
 
 import java.util.Date;
@@ -10,7 +11,7 @@ import java.util.Date;
 @Data
 @TableName("todo_list")
 public class Todo {
-    @TableId
+    @TableId(type = IdType.ASSIGN_UUID)
     private String todoId;        // 主键
 
     @TableField("todo_name")
@@ -24,6 +25,45 @@ public class Todo {
 
     @TableField("is_deleted")
     private Integer isDeleted;    // 删除情况 是=1 否=0
-}
 
+    public String getTodoId() {
+        return todoId;
+    }
+
+    public void setTodoId(String todoId) {
+        this.todoId = todoId;
+    }
+
+    public String getTodoName() {
+        return todoName;
+    }
+
+    public void setTodoName(String todoName) {
+        this.todoName = todoName;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Integer getIsFinished() {
+        return isFinished;
+    }
+
+    public void setIsFinished(Integer isFinished) {
+        this.isFinished = isFinished;
+    }
+
+    public Integer getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Integer isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+}
 
