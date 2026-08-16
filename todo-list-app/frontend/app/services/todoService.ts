d3474a255e5096f68025cff2b6331c74a5eb1dff
vincentic <limingxin0729@foxmail.com>
2026-08-16 16:33:03 +0800

    Wrap todo frontend requests

--- todo-list-app/frontend/app/services/todoService.ts
+++ todo-list-app/frontend/app/services/todoService.ts
@@ -1,35 +1,41 @@
 import { Todo, ApiResponse } from "../types/todo";
 import { TODO_API } from "../apiConfig";
 
+async function request<T>(url: string, init?: RequestInit): Promise<ApiResponse<T>> {
+  const res = await fetch(url, init);
+  const data = (await res.json()) as ApiResponse<T>;
+
+  if (!res.ok) {
+    throw new Error(data.message || `Request failed with status ${res.status}`);
+  }
+
+  return data;
+}
+
 export async function getTodos(): Promise<ApiResponse<Todo[]>> {
-  const res = await fetch(TODO_API.list);
-  return await res.json();
+  return request<Todo[]>(TODO_API.list);
 }
 
 export async function getTodo(id: string): Promise<ApiResponse<Todo>> {
-  const res = await fetch(TODO_API.get(id));
-  return await res.json();
+  return request<Todo>(TODO_API.get(id));
 }
 
 export async function addTodo(todo: Partial<Todo>): Promise<ApiResponse<boolean>> {
-  const res = await fetch(TODO_API.add, {
+  return request<boolean>(TODO_API.add, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(todo),
   });
-  return res.json();
 }
 
 export async function updateTodo(id: string, todo: Partial<Todo>): Promise<ApiResponse<boolean>> {
-  const res = await fetch(TODO_API.update(id), {
+  return request<boolean>(TODO_API.update(id), {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(todo),
   });
-  return res.json();
 }
 
 export async function deleteTodo(id: string): Promise<ApiResponse<boolean>> {
-  const res = await fetch(TODO_API.delete(id));
-  return await res.json();
+  return request<boolean>(TODO_API.delete(id));
 }
